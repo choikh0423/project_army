a40b0c628e3e4f1afd010b18f1a39a8d19978c71
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2021-02-17 15:40:16 +0900

    Update 1-프로그래머즈-문자열내림차순배치하기.py

--- Kyu/1-프로그래머즈-문자열내림차순배치하기.py
+++ Kyu/1-프로그래머즈-문자열내림차순배치하기.py
@@ -1,1 +1,21 @@
+# 시작시간: ---
+# 종료시간: ---
 
+# 1차 시도 - O(?)
+def solution(s):
+    answer = ''
+    # Insertion Sort()
+    for i in range(len(s)):
+        count = i
+        if answer == '':
+            answer = s[i]
+        else:
+            while count > 0 and s[i] > answer[count-1]:
+                count -= 1
+            answer = answer[0:count] + s[i] + answer[count:]
+                
+    return answer
+  
+
+
+# 2차 시도(모범 답안 참고) - O(?)
