9ded287d3168bb6ca28221a8ed8c95bbdca2a2c0
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2021-02-17 16:19:02 +0900

    Update 1-프로그래머즈-문자열내림차순배치하기.py

--- Kyu/1-프로그래머즈-문자열내림차순배치하기.py
+++ Kyu/1-프로그래머즈-문자열내림차순배치하기.py
@@ -14,9 +14,17 @@ def solution(s):
             while count > 0 and s[i] > answer[count-1]:
                 count -= 1
             answer = answer[0:count] + s[i] + answer[count:]
-                
     return answer
-  
-
 
 # 2차 시도(모범 답안 참고) - O(?)
+def solution(s):
+    list_s = list(s)
+    list_s.sort(reverse=True)
+    answer = ""
+    for i in list_s:
+        answer = answer + i
+    return answer
+
+# 3차 시도(모범 답안 참고) - O(?)
+def solution(s):
+    return ''.join(sorted(s, reverse=True))
