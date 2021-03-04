508c575093d0d342a14524631168a8e2da9052f3
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2021-03-04 19:45:12 +0900

    Create 0-main.py

---
+++ Kyu/DataStruct/sorting/0-main.py
@@ -0,0 +1,17 @@
+# This file is for testing the sorting algorithms
+
+from insertionSort import InsertionSort
+import unittest
+
+# Runs all the sorting algorithms
+test1 = [2,1]
+test2 = [5,2,3,1,4]
+test3 = []
+
+InsertionSort(test1)
+InsertionSort(test2)
+InsertionSort(test3)
+
+print(test1)
+print(test2)
+print(test3)
